c33aae60aef0f38002f20bde9ce84e49343ac4d3
tomasyovino <tomasyovino@gmail.com>
2023-09-26 11:18:42 -0300

    Add notifier to payment controller

--- Controller/PaymentController.php
+++ Controller/PaymentController.php
@@ -1,22 +1,25 @@
 <?php
 include_once '../Services/PaymentService.php';
+include_once '../Services/NotificatorService.php';
 include_once "../Services/Strategy/PaymentService/BankTransferStrategy.php";
 include_once "../Services/Strategy/PaymentService/CreditCardStrategy.php";
 
 class PaymentController
 {
     private $paymentService;
+    private $notificatorService;
 
-    public function __construct(PaymentService $paymentService)
+    public function __construct(PaymentService $paymentService, NotificatorService $notificatorService)
     {
         $this->paymentService = $paymentService;
+        $this->notificatorService = $notificatorService;
     }
 
-    public function processPayment($amount)
+    public function processPayment($id, $amount)
     {
         $this->paymentService->paymentProcess($amount);
-
-        $message = "Su compra ha sido procesada correctamente";
-        // $this->observerService->notify($message);
+        
+        $order = "Su compra ha sido procesada correctamente";
+        $this->notificatorService->sendOrder($id, $order);
     }
 }
